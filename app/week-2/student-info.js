0367f4c9c251060e3a7cde9cc44ca138dac2ee47
RileyLearner <riley.johnson@edu.sait.ca>
2025-09-09 14:21:13 -0600

    Complete Week 2
    - Changed the root page.js
    - 'Week-2' page was created
    - Component 'Student Info' was made

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+    return(
+        <div>
+            <p>My name is Riley Johnson</p>
+            <Link href="https://github.com/RileyLearner/cprg306-assignments">Github Repository</Link>
+        </div>
+
+    );
+} 
